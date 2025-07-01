0f9809913251b0193897923fbe88f795af363f33
Ilias van den Heuvel <i.v.d.heuvel1@student.tue.nl>
2025-07-01 14:19:20 +0200

    o

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,7 +1,7 @@
-export const Logo = () => import('../..\\components\\Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
-export const Person = () => import('../..\\components\\Person.vue' /* webpackChunkName: "components/person" */).then(c => wrapFunctional(c.default || c))
-export const Photo = () => import('../..\\components\\Photo.vue' /* webpackChunkName: "components/photo" */).then(c => wrapFunctional(c.default || c))
-export const PhotoScatter = () => import('../..\\components\\PhotoScatter.vue' /* webpackChunkName: "components/photo-scatter" */).then(c => wrapFunctional(c.default || c))
+export { default as Logo } from '../..\\components\\Logo.vue'
+export { default as Person } from '../..\\components\\Person.vue'
+export { default as Photo } from '../..\\components\\Photo.vue'
+export { default as PhotoScatter } from '../..\\components\\PhotoScatter.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
